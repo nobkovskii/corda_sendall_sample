cc3d648a8c5516ae086b953e001b54c3a0b903b3
Nobkovskii <nobuhiro_yokoyama@gfam.co.jp>
2020-07-21 16:14:33 +0900

    [ mod ] Txを発行出来るように修正

--- workflows/src/main/kotlin/com/template/flows/Flows.kt
+++ workflows/src/main/kotlin/com/template/flows/Flows.kt
@@ -25,26 +25,52 @@ class Initiator(private val data: String) : FlowLogic<Unit>() {
     override fun call() {
         val start = System.currentTimeMillis()
         println()
-        println("[INITIATOR START]")
+        println("[INITIATOR] START")
         val b = toParty("O=PartyB,L=New York,C=US")
         val c = toParty("O=PartyC,L=New York,C=US")
         val d = toParty("O=PartyD,L=New York,C=US")
         val e = toParty("O=PartyE,L=New York,C=US")
 
-        println("[INITIATE FLOW]")
+        println("[INITIATE FLOW] START")
         val sessionB = initiateFlow(b)
         val sessionC = initiateFlow(c)
         val sessionD = initiateFlow(d)
         val sessionE = initiateFlow(e)
+        println("[INITIATE FLOW] END")
 
-        println("[SEND START]")
+        println("[SEND] START")
         sendAll(data, setOf(sessionB, sessionC, sessionD, sessionE))
-        println("[SEND END]")
+        println("[SEND] END")
 
-        println("[INITIATOR END]")
+        println("[TRANSACTION BUILDER] START")
+        val notary = serviceHub.networkMapCache.notaryIdentities[0]
+        val output = TemplateState(data, listOf(ourIdentity, b, c))
+        val cmd = Command(TemplateContract.Commands.Action(), listOf(ourIdentity.owningKey, b.owningKey, c.owningKey))
+        val txBuilder = TransactionBuilder(notary)
+                .addCommand(cmd)
+                .addOutputState(output)
+        println("[TRANSACTION BUILDER] END")
+
+        println("[TRANSACTION VERIFY] START")
+        txBuilder.verify(serviceHub)
+        println("[TRANSACTION VERIFY] END")
+
+        println("[SIGN INITIAL TRANSACTION] START")
+        val signedTx = serviceHub.signInitialTransaction(txBuilder)
+        println("[SIGN INITIAL TRANSACTION] END")
+
+        println("[COLLECT SIGN] START")
+        val fullySignedTx = subFlow(CollectSignaturesFlow(signedTx, setOf(sessionB, sessionC)))
+        println("[COLLECT SIGN] END")
+
+        println("[FINALITY FLOW] START")
+        subFlow(FinalityFlow(fullySignedTx, setOf(sessionB, sessionC)))
+        println("[FINALITY FLOW] END")
+
+        println("[INITIATOR] END")
         println()
         val end = System.currentTimeMillis()
-        println("TIME : ${end-start} ms")
+        println("TIME : ${end - start} ms")
     }
 
     private fun toParty(name: String): Party {
@@ -59,12 +85,25 @@ class Responder(val counterPartySession: FlowSession) : FlowLogic<Unit>() {
     @Suspendable
     override fun call() {
         println()
-        println("[RESPONDER START]")
+        println("[RESPONDER] START")
 
+        println("[RECEIVE ALL] START")
         val list = receiveAll(String::class.java, listOf(counterPartySession))
         list[0].unwrap { data -> println(data) }
+        println("[RECEIVE ALL] END")
 
-        println("[RESPONDER END]")
+        println("----------------------------")
+
+        println("[SIGN TRANSACTION] START")
+        val signTransactionFlow = object : SignTransactionFlow(counterPartySession) {
+            override fun checkTransaction(stx: SignedTransaction) = requireThat {}
+        }
+        val txId = subFlow(signTransactionFlow).id
+        subFlow(ReceiveFinalityFlow(counterPartySession, expectedTxId = txId))
+        println("[SIGN TRANSACTION] $txId")
+        println("[SIGN TRANSACTION] END")
+        
+        println("[RESPONDER] END")
         println()
     }
 }
